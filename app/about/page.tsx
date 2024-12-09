966608e0e0f9be7c0497c9915596bf89098af760
Bharath <bharathj0410@gmail.com>
2024-12-09 22:41:35 +0530

    aa

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -23,7 +23,7 @@ export default function AboutPage() {
         <p className="h-[0.4rem] bg-primary-300 rounded-full mt-1 w-[5rem] sm:mb-2"></p>
         <div className="overflow-hidden">
           <Image
-            src={"/AboutUs.jpg"}
+            src="/about-us.jpg"
             alt="about"
             width={5000}
             height={0}
